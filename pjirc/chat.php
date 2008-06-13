c7367b5548d5d78af9ad60b7103c4d95d2c3af4d
Cinquante <Cinquante@starshine-online.com>
2008-06-13 11:13:45 +0000

    * le tchat

---
+++ pjirc/chat.php
@@ -0,0 +1,8 @@
+	<applet code=IRCApplet.class archive="irc.jar,pixx.jar" width=640 height=400>
+	<param name="CABINETS" value="irc.cab, securedirc.cab, pixx.cab">
+	<param name="nick" value="">
+	<param name="name" value="Starshine User">
+	<param name="host" value="irc.quakenet.org">
+	<param name="gui" value="pixx">
+     <param name="command1" value="/join #starshine-online">
+	</applet>
