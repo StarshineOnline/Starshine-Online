b4841ed894733ff08f17c730f647b7d9565c90a9
starshine <starshine@starshine-online.com>
2009-06-29 15:00:11 +0000

    Chan de race, pour nos blaireaux de joueurs préférés ...

--- acces_chat.php
+++ acces_chat.php
@@ -3,12 +3,16 @@ include('inc/fp.php');
 $joueur = recupperso($_SESSION['ID']);
 //Affichage du chat
 
-$url = "http://widget.mibbit.com/?settings=a0f3506cf53bb4bdc5476642cc300fb2&server=irc.quakenet.org&channel=%23starshine-online&noServerTab=false&autoConnect=true&nick=".$joueur['nom'];
+$base = "http://widget.mibbit.com/?settings=a0f3506cf53bb4bdc5476642cc300fb2&server=irc.quakenet.org&noServerTab=true&autoConnect=true&nick=".$joueur['nom'];
+
+$url = $base.'&channel=%23starshine-online';
+$url_roy = $base.'&channel=%23starshine-online-'.$joueur['race'];
 
 ?>
 <script type="text/javascript">
 
 </script>
 <a href="<?= $url ?>" target="_new">Chat (nouvelle fen&ecirc;tre)</a><br />
+<a href="<?= $url_roy ?>" target="_new">Chat de royaume (nouvelle fen&ecirc;tre)</a><br />
 <a href="#" onClick="javascript:showChat('<?= $url ?>');" id="chatlink">Chat (Ingame)</a><br />
 <iframe id="chatframe" style="width: 800px; height: 470px; display: none;" src="" />
