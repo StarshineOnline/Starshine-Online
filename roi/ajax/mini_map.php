c08fba04603a8e06009f5b6a4b0f59a7e239a964
cinquante <cinquante@starshine-online.com>
2009-08-15 16:36:04 +0000

    de la merde

--- roi/ajax/mini_map.php
+++ roi/ajax/mini_map.php
@@ -7,23 +7,12 @@ include(root.'haut_ajax.php');
 
 $joueur = new perso($_SESSION['ID']);
 
-$R = get_royaume_info($joueur->get_race(), $Trace[$joueur->get_race()]['numrace']);
+$royaume = new royaume($Trace[$joueur->get_race()]['numrace']);
 
 
-$W_distance = detection_distance($W_case,convert_in_pos($joueur->get_x(), $joueur->get_y()));
-
-$W_coord = convert_in_coord($W_case);
-
-$batiments = array();
-$dimensions = dimension_map($_POST['x'], $_POST['y'], 8);
-$requete = "SELECT x, y, hp, nom, type, image FROM construction WHERE royaume = ".$R['ID']." AND x >= ".$dimensions['xmin']." AND x <= ".$dimensions['xmax']." AND y >= ".$dimensions['ymin']." AND y <= ".$dimensions['ymax'];
-$req = $db->query($requete);
-while($row = $db->read_assoc($req))
-{
-	$batiments[convert_in_pos($row['x'], $row['y'])] = $row;
-}	
 $map = new map($_POST['x'], $_POST['y'], 8, '../', false, 'low');
-$map->set_batiment($batiments);
+$map->get_drapeau();
+$map->get_batiment();
 $map->quadrillage = true;
 $map->affiche();
 
