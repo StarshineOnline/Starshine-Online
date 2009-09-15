24d8ef4e4b4c31b68123c3310b0c8980959cdce9
archastra <archastra@starshine-online.com>
2009-09-15 22:59:58 +0000

    - La même deuxième service

--- info_monstre.php
+++ info_monstre.php
@@ -29,7 +29,7 @@ $coeff = floor($survie / $monstre->get_level());
 	<p>Niveau : <?php echo $monstre->get_level(); ?><?php if($coeff >= 7) echo ' - Type : '.$monstre->get_type(); ?>&nbsp;&nbsp;&nbsp;&nbsp;
 	<?php
 	if(!$joueur->is_buff('repos_sage') OR !$joueur->is_buff('bloque_attaque')) echo '<a href="attaque.php?type=monstre&id_monstre='.$map_monstre->get_id().'" onclick="return envoiInfo(this.href, \'information\')"><img src="image/interface/attaquer.png" alt="Combattre" title="Attaquer ('.($pa_attaque - $reduction_pa).' PA)" style="vertical-align : middle;" /> </a>';
-	if($joueur->get_sort_jeu() != '') echo ' <a href="sort.php?poscase='.$map_monstre->get_pos().'&id_monstre='.$monstre->get_id().'&amp;type=monstre" onclick="return envoiInfo(this.href, \'information\')"><img src="image/sort_hc_icone.png" title="Lancer un sort" alt="Lancer un sort" style="vertical-align : middle;" /></a>';
+	if($joueur->get_sort_jeu() != '') echo ' <a href="sort.php?poscase='.$map_monstre->get_pos().'&id_monstre='.$map_monstre->get_id().'&amp;type=monstre" onclick="return envoiInfo(this.href, \'information\')"><img src="image/sort_hc_icone.png" title="Lancer un sort" alt="Lancer un sort" style="vertical-align : middle;" /></a>';
 	echo '<a href="informationcase.php?case='.$W_case.'" onclick="return envoiInfo(this.href, \'information\')"><img src="image/interface/retour.png" alt="Retour" title="Retour à l\'information case" style="vertical-align : middle;" /></a>';
 	echo '<br />';
 	//Listing des debuffs
