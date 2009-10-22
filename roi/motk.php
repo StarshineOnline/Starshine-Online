2d7df5e8592c19a8dd1859f88742aed546748ab2
Cinquante <Cinquante@starshine-online.com>
2009-10-22 08:56:05 +0000

    Correction des messages du roi

--- roi/motk.php
+++ roi/motk.php
@@ -11,40 +11,39 @@ elseif(array_key_exists('message', $_GET))
 {
 	if ($_GET['message'] != '')
 	{
-		if($royaume->set_motk(sSQL($_GET['message'])))
-		{
-			echo '<h6>Message du roi bien modifiée !</h6>';
-		}
-		else
-		{
-			echo('<h5>Erreur lors de l\'envoi du message</h5>');
-		}
+		$royaume->set_propagande(sSQL($_GET['message']));
+		echo '<h6>Propagande bien modifiée !</h6>';
 	}
 	else
 	{
 		echo '<h5>Vous n\'avez pas saisi de message</h5>';
 	}
 }
-else
-{
 	echo "<div id='propagande'>";
 	//Message actuel
 	$royaume->get_motk();
 	$message = transform_texte($royaume->motk->get_message());
+	$message = str_replace('[br]', '<br />', $message);
+	//$message = $amessage.$message;
+	$message = eregi_replace("\[img\]([^[]*)\[/img\]", '<img src=\\1 title="\\1">', $message );
+	$message = eregi_replace("\[b\]([^[]*)\[/b\]", '<strong>\\1</strong>', $message );
+	$message = eregi_replace("\[i\]([^[]*)\[/i\]", '<i>\\1</i>', $message );
+	$message = eregi_replace("\[url\]([^[]*)\[/url\]", '<a href="\\1">\\1</a>', $message );
+	$message = str_replace("[/color]", "</span>", $message);
+	$regCouleur = "\[color= ?(([[:alpha:]]+)|(#[[:digit:][:alpha:]]{6})) ?\]";
+	$message = eregi_replace($regCouleur, "<span style=\"color: \\1\">", $message);
+	
 	if (empty($message)){$message = "Aucun message du roi pour l'instant";}
 	echo "<fieldset>";
 	echo "<legend>Message du roi actuel</legend>
-	<div id='message_roi' onclick=\"$('message_roi_edit').show();$('message_roi').hide();\">
+	<div id='message_roi' onclick=\"$('#message_roi_edit').show();$('#message_roi').hide();\">
 	".$message."
 	</div>";
 	?>
 	<div id='message_roi_edit' style='display:none;'>
 		<textarea name="message" id="messageid" cols="90" rows="12"><?php echo htmlspecialchars(stripslashes(($royaume->motk->get_message()))); ?></textarea><br />
-		<input type="button" value="Envoyer" onclick="envoiInfo('motk.php?message=' + $('messageid').value, 'message_confirm');envoiInfo('motk.php', 'contenu_jeu');" />
+		<input type="button" value="Envoyer" onclick="envoiInfo('motk.php?message=' + encodeURIComponent($('#messageid').val()), 'contenu_jeu');" />
 	</div>
 	
 	</fieldset>
 	</div>
-<?php
-}
-?>
--- roi/propagande.php
+++ roi/propagande.php
@@ -7,11 +7,11 @@ include_once(root.'fonction/messagerie.inc.php');
 
 if($joueur->get_rang_royaume() != 6)
 	echo '<p>Cheater</p>';
-elseif(array_key_exists('message', $_POST))
+elseif(array_key_exists('message', $_GET))
 {
-	if ($_POST['message'] != '')
+	if ($_GET['message'] != '')
 	{
-		$royaume->set_propagande($_POST['message']);
+		$royaume->set_propagande(sSQL($_GET['message']));
 		echo '<h6>Propagande bien modifiée !</h6>';
 	}
 	else
@@ -19,12 +19,21 @@ elseif(array_key_exists('message', $_POST))
 		echo '<h5>Vous n\'avez pas saisi de message</h5>';
 	}
 }
-else
-{
 	echo "<div id='propagande'>";
 	//Message actuel
 	$royaume->get_motk();
 	$message = transform_texte($royaume->motk->get_propagande());
+
+	$message = str_replace('[br]', '<br />', $message);
+	//$message = $amessage.$message;
+	$message = eregi_replace("\[img\]([^[]*)\[/img\]", '<img src=\\1 title="\\1">', $message );
+	$message = eregi_replace("\[b\]([^[]*)\[/b\]", '<strong>\\1</strong>', $message );
+	$message = eregi_replace("\[i\]([^[]*)\[/i\]", '<i>\\1</i>', $message );
+	$message = eregi_replace("\[url\]([^[]*)\[/url\]", '<a href="\\1">\\1</a>', $message );
+	$message = str_replace("[/color]", "</span>", $message);
+	$regCouleur = "\[color= ?(([[:alpha:]]+)|(#[[:digit:][:alpha:]]{6})) ?\]";
+	$message = eregi_replace($regCouleur, "<span style=\"color: \\1\">", $message);
+
 	if (empty($message)){$message = "Aucune propagande pour l'instant";}
 	echo "<fieldset>";
 	echo "<legend>Propagande actuelle</legend>
@@ -34,13 +43,11 @@ else
 	?>
 	<div id='message_propagande_edit' style='display:none;'>
         <form method="post" action="propagande.php?direction=propagande" id="formPropagande">
-			<textarea name="message" id="messageid" cols="90" rows="12"><?php echo htmlspecialchars(stripslashes($royaume->motk->get_propagande())); ?></textarea><br />
-			<input type="button" value="Envoyer" onclick="envoiFormulaire('formPropagande', 'message_confirm');envoiInfo('propagande.php', 'contenu_jeu');" />
+			<textarea name="message" id="message" cols="90" rows="12"><?php echo htmlspecialchars(stripslashes($royaume->motk->get_propagande())); ?></textarea><br />
+			<input type="button" value="Envoyer" onclick="envoiInfo('propagande.php?message='+encodeURIComponent($('#message').val()), 'contenu_jeu');" />
 		</form>
 	</div>
 	
 	</fieldset>
 	</div>
-<?php
-}
-?>
+
