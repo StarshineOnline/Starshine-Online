1e2aca961220626e0e3d20e40cb056cad372a210
bastien <bastien@starshine-online.com>
2009-12-08 17:54:03 +0000

    correction opera 10, message IE

--- arenes/arene_poll.js
+++ arenes/arene_poll.js
@@ -7,19 +7,31 @@ var time = 2000;
 
 function begin_poll()
 {
-	$.ajax({
-		type: "GET",
-		url: "arene.xsl",
-		success: function(msg) {
-			try {
-				xsltProcessor = new XSLTProcessor();
-				xsltProcessor.importStylesheet(msg);
-				t = setTimeout("do_poll()", 3000);
-			} catch (e) {
-				alert('Pas de refresh auto possible: import XSL' + e);
+	if (window.XSLTProcessor) {					
+		// Mozilla, Opera, etc.
+		$.ajax({
+			type: "GET",
+			url: "arene.xsl",
+			dataType: "xml",
+			success: function(msg) {
+				try {
+					xsltProcessor = new XSLTProcessor();			
+				} catch (e) {
+					alert('Pas de refresh auto possible: creation XSLTProcessor: ' + e);
+					return;
+				}
+				try {
+					xsltProcessor.importStylesheet(msg);
+					t = setTimeout("do_poll()", time);
+				} catch (e) {
+					alert('Pas de refresh auto possible: import XSL: ' + e + ' - msg: ' + msg);
+				}
 			}
-		}
-	});
+		});
+	}
+	else {
+		alert('Pas de refresh auto possible: navigateur incompatible');
+	}
 }
 
 function do_poll()
@@ -37,11 +49,12 @@ function do_poll()
 function do_xsl(msg)
 {
 	try {
-		var resultDocument = xsltProcessor.transformToDocument(msg);
+		var resultDocument;
+		resultDocument = xsltProcessor.transformToDocument(msg);
 		var contents = resultDocument.getElementById("div_map").innerHTML;
 		$("div.div_map").html(contents);
 	} catch (e) {
-		alert('Pas de refresh auto possible: processing XSL' + e);
+		alert('Pas de refresh auto possible: processing XSL: ' + e);
 		time = time * 10;
 	}
 }
