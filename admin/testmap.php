859bb3fddb15cb17f9238bb0a19587ad91181a98
Cinquante <Cinquante@starshine-online.com>
2010-01-04 16:56:28 +0000

    j ai fait de la merde

--- admin/testmap.php
+++ admin/testmap.php
@@ -3,11 +3,11 @@ if (file_exists('../root.php'))
   include_once('../root.php');
 $admin =true;
 include_once(root.'haut.php');
-//include_once(root.'inc/verif_log_admin.inc.php');
+include_once(root.'inc/verif_log_admin.inc.php');
 ?>
 <a href="admin_2.php">Revenir à l'administration</a>
 <?php
-$map = new map(95, 95, 95);
+$map = new map(95, 95, 95,'../');
 
 $map->get_pnj();
 $map->get_joueur('neutre');
