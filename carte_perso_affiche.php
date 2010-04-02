f24c723e7f5761766d31f11dbeef0fbfb562925d
Cinquante <Cinquante@starshine-online.com>
2010-04-02 12:36:48 +0000

    Mise à jour des textures low

--- carte_perso_affiche.php
+++ carte_perso_affiche.php
@@ -1,22 +1,6 @@
 <?php
 if (file_exists('root.php'))
   include_once('root.php');
-  include_once(root.'inc/fp.php');
-
 ?><div id="carte">
-<?php
-
-$RqMapTxt = "SELECT id,info FROM map 
-				 ORDER BY id;";
-$RqMap = $db->query($RqMapTxt);
-while($objMap = $db->read_object($RqMap))
-{
-	echo $objMap->id;
-
-
-}
-
-?>
-
-	
+	<img src="carte_perso.php" alt="" />
 </div>
