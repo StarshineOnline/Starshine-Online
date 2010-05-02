bfb7045c1637e595e181fdaa3e2b880ef7a0bbcf
bastien <bastien@starshine-online.com>
2010-05-02 16:55:03 +0000

    UNIQUE INDEX dans les options

--- sql/update_20100502.sql
+++ sql/update_20100502.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `map_zone`
 ADD `ordre` TINYINT NOT NULL DEFAULT '0'
 COMMENT 'Priorité du calque par rapport aux autre (si chevauchment)';
+
+ALTER TABLE `options` add UNIQUE KEY `id_perso` (`id_perso`,`nom`) ;
