06efbef0eacd95a903634f72903f1a58ae3fb80b
dalai <dalai@starshine-online.com>
2010-06-01 16:38:58 +0000

    * again

--- bas.php
+++ bas.php
@@ -7,6 +7,18 @@ if (file_exists('root.php'))
 ?>
 </div>
 <div id="preload-images"></div>
+	<!-- Piwik -->
+<script type="text/javascript">
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://www.starshine-online.com/piwik/" : "http://www.starshine-online.com/piwik/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://www.starshine-online.com/piwik/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Tag -->
 </body>
 
 </html>
