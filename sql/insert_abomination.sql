f9cd67796b172d642b6d2a5b1b7ace9257d960fa
bastien <bastien@starshine-online.com>
2010-07-02 13:10:35 +0000

    sql correct

--- sql/insert_abomination.sql
+++ sql/insert_abomination.sql
@@ -1,4 +1,4 @@
-delete from map_monstre where lib like 'abomination_gob_%' OR lib like 'abomination_tentacule_haut_%';
+delete from map_monstre where type in (select id from monstre where lib like 'abomination_gob_%' OR lib like 'abomination_tentacule_haut_%');
 
 insert into map_monstre values(null, (select id from monstre where lib = 'abomination_tentacule_haut_gauche'), 16, 293, 1000000, 2279168080);
 insert into map_monstre values(null, (select id from monstre where lib = 'abomination_tentacule_haut_milieu'), 17, 293, 1000000, 2279168080);
@@ -14,4 +14,3 @@ insert into map_monstre values(null, (select id from monstre where lib = 'abomin
 insert into map_monstre values(null, (select id from monstre where lib = 'abomination_gob_bas_ventre'), 17, 296, 1000000, 2279168080);
 insert into map_monstre values(null, (select id from monstre where lib = 'abomination_gob_patte_droite'), 18, 296, 1000000, 2279168080);
 insert into map_monstre values(null, (select id from monstre where lib = 'abomination_gob_bras_droit'), 19, 296, 1000000, 2279168080);
-
