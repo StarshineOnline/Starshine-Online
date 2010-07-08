cc9bbfc30dd8fb85174b84e1842aecfa7e939bef
bastien <bastien@starshine-online.com>
2010-07-08 12:34:22 +0000

    pnj plan de karn

---
+++ sql/update_20100708.sql
@@ -0,0 +1,12 @@
+-- plan de karn
+INSERT INTO `pnj` (
+`id` ,
+`nom` ,
+`image` ,
+`x` ,
+`y` ,
+`texte`
+)
+VALUES (
+NULL , 'Rejeton d''Aâzgruthh', 'Rejeton_aazgruthh', '42', '216', 'Greuuu !'
+);
