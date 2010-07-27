00f97d07f571a1552687b8fbe6343a13d290a21b
bastien <bastien@starshine-online.com>
2010-07-27 16:28:08 +0000

    GROS GROS BUG svn diff

--- sql/update_20100727.sql
+++ sql/update_20100727.sql
@@ -24,4 +24,4 @@ VALUES (
 NULL , 'Nostalgie de Karn', 'Affecte la RM (division par 1.%effet2%) Dégats + %effet%', '0', 'nostalgie_karn', 'sort_element', 'puissance', '0', '0', '1', '10', '50', '0', '4', '', '0', '1', '99', ''
 );
 
-update `monstre` set `action` = '#09=3@~179;#10°berzeker@_99;#09=20@_84;#14<5@_79;#14<5@_77' ;
+update `monstre` set `action` = '#09=3@~179;#10°berzeker@_99;#09=20@_84;#14<5@_79;#14<5@_77' where id = 145;
