beef686eebe2d6e16eddea322e590ed4c74bd53e
bastien <bastien@starshine-online.com>
2010-07-28 10:01:51 +0000

    script correct pour plan de karn

--- sql/update_20100727.sql
+++ sql/update_20100727.sql
@@ -24,4 +24,4 @@ VALUES (
 NULL , 'Nostalgie de Karn', 'Affecte la RM (division par 1.%effet2%) Dégats + %effet%', '0', 'nostalgie_karn', 'sort_element', 'puissance', '0', '0', '1', '10', '50', '0', '4', '', '0', '1', '99', ''
 );
 
-update `monstre` set `action` = '#09=3@~179;#10°berzeker@_99;#09=20@_84;#14<5@_79;#14<5@_77' where id = 145;
+update `monstre` set `action` = '#09=3@~179;#14<3µ#10°posture@~101;#11°appel_tenebreµ#09<15@~70;#14<3@~173;#14<3@~44;#14<3@~92;#14<3@~128;#14<3@~122' where id = 145;
