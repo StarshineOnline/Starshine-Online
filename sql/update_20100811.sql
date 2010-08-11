16c8ae7588365b2e6d697361176fe8e896241321
bastien <bastien@starshine-online.com>
2010-08-11 07:48:32 +0000

    pop ambibe et serviteur d'adenaois

---
+++ sql/update_20100811.sql
@@ -0,0 +1,3 @@
+-- pop monstres de donj
+update monstre set spawn_loc='p6-221;p7-221;p8-221;p9-221;p10-221;p11-221;p12-221;p12-222;p12-223;p12-224;p12-225;p12-226;p12-227;p13-224;p13-225;p13-226;p13-227;p14-227' where id = 143;
+update monstre set spawn_loc = 'p54-292;p54-293;p54-294;p55-292;p55-293;p55-294;p57-293;p56-289;p56-287;p51-286;p52-286;p47-286' where id = 142;
