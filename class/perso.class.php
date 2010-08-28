2241f79f673b60cc38904bbaf8d2c26d45747133
Florp <Florp@starshine-online.com>
2010-08-28 10:08:46 +0000

    - Correction de la formule de hausse des HP (BugID #301)

--- class/perso.class.php
+++ class/perso.class.php
@@ -2785,8 +2785,8 @@ class perso extends entite
 			{
 				$time = time();
 				$nb_maj = floor($temps_maj / $temps_hp);
-				$hp_gagne = $nb_maj * (pow($this->get_vie(true), 0.7) * 1.6);
-				$this->set_hp_max($this->get_hp_max(true) + $hp_gagne);
+				$hp_gagne = $nb_maj * (pow($this->get_vie(true), 0.7) * 2.6);
+				$this->set_hp_max($this->get_hp_maximum() + $hp_gagne);
 				$this->set_maj_hp($this->get_maj_hp() + $nb_maj * $temps_hp);
 				$modif = true;
 			}
