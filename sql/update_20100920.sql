c6f155557a0bd89aff9dd26aee5a484e4df95601
Florp <Florp@starshine-online.com>
2010-09-20 10:39:34 +0000

    modifications pour gestion des batailles, version 3

--- sql/update_20100920.sql
+++ sql/update_20100920.sql
@@ -45,3 +45,6 @@ INSERT INTO `bataille_repere_type` (`id`, `nom`, `description`, `ajout_groupe`, 
 (7, 'Déplacement', 'Ordonner à un ou plusieurs groupe de se rendre à la position indiquée.', 0, ''),
 (8, 'Réparation', 'Ordonner à un ou plusieurs groupe de réparer un bâtiment.', 0, ''),
 (9, 'Construction', 'Ordonner à un ou plusieurs groupe de construire un batiment, une arme de siège, à la position indiquée.', 0, '');
+
+
+ALTER TABLE `bataille_groupe` ADD `id_thread` INT NOT NULL
