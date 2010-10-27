49e95413c1a7cc4020d20cb9445826f0f2bfdb82
bastien <bastien@starshine-online.com>
2010-10-27 20:43:50 +0000

    et le SQL qui va avec ^^

--- sql/update_20101010.sql
+++ sql/update_20101010.sql
@@ -1,3 +1,3 @@
-UPDATE `starshine`.`sort_jeu` SET `lvl_batiment` = '4' WHERE `sort_jeu`.`id` =134;
-UPDATE `starshine`.`sort_jeu` SET `lvl_batiment` = '5' WHERE `sort_jeu`.`id` =135;
-UPDATE `starshine`.`sort_jeu` SET `lvl_batiment` = '5' WHERE `sort_jeu`.`id` =131;
+UPDATE `sort_jeu` SET `lvl_batiment` = '4' WHERE `sort_jeu`.`id` =134;
+UPDATE `sort_jeu` SET `lvl_batiment` = '5' WHERE `sort_jeu`.`id` =135;
+UPDATE `sort_jeu` SET `lvl_batiment` = '5' WHERE `sort_jeu`.`id` =131;
