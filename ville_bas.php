989396ae91b0be8e935656ac8986cbdf8f0c18cd
bastien <bastien@starshine-online.com>
2010-10-30 18:40:05 +0000

    icones de fonction dans les bourgs

--- ville_bas.php
+++ ville_bas.php
@@ -33,6 +33,31 @@ else
 <a href="bureau_quete.php" onclick="return envoiInfo(this.href,'carte')" title="Bureau des quêtes"><img src="image/ville/bureau_des_quete.png" alt="Bureau des quêtes" /></a>
 <?php
 	}
+	if ($batiment->has_bonus('poste')) {
+?>
+<a href="poste.php" onclick="return envoiInfo(this.href,'carte')" title="Poste"><img src="image/ville/poste.png" alt="Poste" /></a>
+<?php
+	}
+	if ($batiment->has_bonus('alchimiste')) {
+?>
+<a href="alchimiste.php" onclick="return envoiInfo(this.href,'carte')" title="Alchimiste"><img src="image/ville/alchimiste.png" alt="Alchimiste" /></a>
+<?php
+	}
+	if ($batiment->has_bonus('teleport')) {
+?>
+<a href="teleport.php" onclick="return envoiInfo(this.href,'carte')" title="Téléportation"><img src="image/ville/teleportation.png" alt="Téléportation" /></a>
+<?php
+	}
+	if ($batiment->has_bonus('royaume')) {
+		$royaume = new royaume($Trace[$joueur->get_race()]['numrace']);
+		if ($joueur->get_grade()->get_id() == 6 // roi
+				OR $joueur->get_id() == $royaume->get_ministre_economie()
+				OR $joueur->get_id() == $royaume->get_ministre_militaire()) {
+?>
+<a href="roi/" title="Gestion du royaume"><img src="image/ville/gestion.png" alt="Gestion du royaume" /></a>
+<?php
+	 }
+	}
 }
 ?>
 
