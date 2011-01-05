f43b41f9f506997d63d6cfb8af939e7b83a1a240
bastien <bastien@starshine-online.com>
2011-01-05 14:12:18 +0000

    achievements du donjon gob

---
+++ sql/update_20110105.sql
@@ -0,0 +1,34 @@
+-- -*- sql -*-
+
+-- Quêtes du gobelin artiste
+INSERT INTO `starshine`.`achievement_type` (
+`id` ,
+`nom` ,
+`description` ,
+`value` ,
+`variable` ,
+`secret`
+)
+VALUES (
+NULL , 'Explorateur du Royaume Gobelin', 'Avoir validé la première quête du gobelin artiste', '1', 'quest_gob', '0'
+), (
+NULL , 'Sauveur de l''art Gobelin', 'Avoir validé la seconde quête du gobelin artiste', '2', 'quest_gob', '0'
+);
+
+-- Abomination
+INSERT INTO `starshine`.`achievement_type` (
+`id` ,
+`nom` ,
+`description` ,
+`value` ,
+`variable` ,
+`secret`
+)
+VALUES (
+NULL , 'Tueur du Roi Gobelin Zewak Rustog', 'Avoir tué le roi gobelin derrière l''abomination', '1', 'kill_gob_king', '0'
+), (
+NULL , 'Marqué par la Terreur Gobeline', 'Avoir été touché par la marque de l''abomination', '1', 'abomination_mark', '0'
+), (
+NULL , 'Survivant de l''Abominable', 'Avoir survécu à un combat contre l''abomination', '2', 'abomination_mark', '0'
+);
+
