9cc0a12dd8af77c921e0eb9e58890295340c1c64
dalai <dalai@starshine-online.com>
2011-01-30 13:14:24 +0000

    Système de dons

---
+++ don.php
@@ -0,0 +1,16 @@
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="FR3RDRQTGWJEE">
+<input type="image" src="https://www.paypal.com/fr_FR/FR/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !">
+<img alt="" border="0" src="https://www.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+
+<!-- Begin Allopass Checkout-Button Code -->
+<script type="text/javascript" src="https://payment.allopass.com/buy/checkout.apu?ids=244083&idd=960025&lang=fr"></script>
+<noscript>
+ <a href="https://payment.allopass.com/buy/buy.apu?ids=244083&idd=960025" style="border:0">
+  <img src="https://payment.allopass.com/static/buy/button/fr/162x56.png" style="border:0" alt="Buy now!" />
+ </a>
+</noscript>
+<!-- End Allopass Checkout-Button Code -->
---
+++ don_allopass.php
@@ -0,0 +1,3 @@
+<noscript>
+    <meta http-equiv="Refresh" content="0;url=https://payment.allopass.com/error.apu?ids=244083&idd=960025">
+</noscript>
