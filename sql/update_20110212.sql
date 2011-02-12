96b38249a1ccd1d3e160dc55ca10136220d3edff
Elettar <Elettar@starshine-online.com>
2011-02-12 20:42:59 +0000

    Un petit oubli

---
+++ sql/update_20110212.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `event_partie` CHANGE `date_debut` `heure_debut` INT( 10 ) NOT NULL ,
+CHANGE `date_fin` `heure_fin` INT( 10 ) NULL DEFAULT NULL
+ALTER TABLE `arenes` ADD `positions` TEXT NOT NULL
