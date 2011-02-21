c183a2661b090a3ed7b658a2bab82e8727e3bdfa
bastien <bastien@starshine-online.com>
2011-02-21 20:51:58 +0000

    bug sql de bob l'eponge -> base pas up to date -> fiche la grouille

--- sql/update_20110221.sql
+++ sql/update_20110221.sql
@@ -1,10 +1,7 @@
-INSERT INTO `achievement_type` (`id`, `nom`, `description`, `value`, `variable`, `secret`, `strong`, `color`) VALUES ('87', 'Piétaille', 'Être rang 2', '0', 'rang_2', '0', '0', NULL), ('88', 'Vétéran', 'Être rang 3', '0', 'rang_3', '0', '0', NULL), ('89', 'Spec ops', 'Être rang 4', '0', 'rang_4', '0', '0', NULL);
+INSERT INTO `achievement_type` (`id`, `nom`, `description`, `value`, `variable`, `secret`, `strong`, `color`) VALUES (null, 'Piétaille', 'Être rang 2', '0', 'rang_2', '0', '0', NULL), (null, 'Vétéran', 'Être rang 3', '0', 'rang_3', '0', '0', NULL), (null, 'Spec ops', 'Être rang 4', '0', 'rang_4', '0', '0', NULL);
 
+INSERT INTO `achievement` (`id_perso`, `id_achiev`) SELECT perso.`ID`, (select id from achievement_type where `variable` = 'rang_2') id_achiev FROM `perso`, classe WHERE perso.classe = classe.nom and classe.rang > 1;
 
-INSERT INTO `achievement` (`id_perso`, `id_achiev`) SELECT `ID`, 87 FROM `perso` WHERE ( `classe_id` > 2 );
+INSERT INTO `achievement` (`id_perso`, `id_achiev`) SELECT perso.`ID`, (select id from achievement_type where `variable` = 'rang_3') id_achiev FROM `perso`, classe WHERE perso.classe = classe.nom and classe.rang > 2;
 
-INSERT INTO `achievement` (`id_perso`, `id_achiev`) SELECT `ID`, 88 FROM `perso` WHERE ( `classe_id` > 8 AND `classe_id` < 17 AND `classe_id` <> 15);
-INSERT INTO `achievement` (`id_perso`, `id_achiev`) SELECT `ID`, 88 FROM `perso` WHERE ( `classe_id` > 27 AND `classe_id` < 40 );
-
-INSERT INTO `achievement` (`id_perso`, `id_achiev`) SELECT `ID`, 89 FROM `perso` WHERE ( `classe_id` > 33 AND `classe_id` < 40 );
-INSERT INTO `achievement` (`id_perso`, `id_achiev`) SELECT `ID`, 89 FROM `perso` WHERE ( `classe_id` > 16 AND `classe_id` < 24 );
+INSERT INTO `achievement` (`id_perso`, `id_achiev`) SELECT perso.`ID`, (select id from achievement_type where `variable` = 'rang_4') id_achiev FROM `perso`, classe WHERE perso.classe = classe.nom and classe.rang > 3;
