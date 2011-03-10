7efe83b9f17ee3d08b60e1560cb644ec25465768
maeryth <maeryth@starshine-online.com>
2011-03-10 18:28:02 +0000

    achievement

---
+++ sql/update_20110310.sql
@@ -0,0 +1,1 @@
+INSERT INTO achievement_type (id, nom, description, value, variable, secret, strong, color) VALUES (NULL, 'Aurum potesta est', 'Avoir %value% stars', '1000000', 'stars', '0', '0', NULL)
