11072f2cf154ddef533103781d61c631cdb54724
bastien <bastien@starshine-online.com>
2011-03-24 11:09:28 +0000

    - doublon

--- sql/update_20110324.sql
+++ sql/update_20110324.sql
@@ -2,7 +2,7 @@ INSERT INTO achievement_type (id, nom, description, value, variable, secret, str
 (NULL, 'Ain t no rest for the wicked', 'Avoir infligé %value% coups critiques', '2000', 'critique', '0', '0', NULL);
 
 INSERT INTO achievement_type (id, nom, description, value, variable, secret, strong, color) VALUES 
-(NULL, 'Les oreilles et la queue ', 'Avoir infligé %value% coups critiques', '2000', 'critique', '0', '0', NULL);
+(NULL, 'Les oreilles et la queue ', 'Avoir infligé %value% coups critiques', '1000', 'critique', '0', '0', NULL);
 
 INSERT INTO achievement_type (id, nom, description, value, variable, secret, strong, color) VALUES 
 (NULL, 'Sous le baby ! ', 'Avoir infligé %value% coups critiques', '500', 'critique', '0', '0', NULL);
