00d2f7e66fb2a999f3cb98fa79385bf97954347e
bastien <bastien@starshine-online.com>
2011-05-18 21:48:09 +0000

    sql mieux

--- sql/update_20110518.sql
+++ sql/update_20110518.sql
@@ -1,6 +1,7 @@
 -- mode: sql; codin: utf-8; -*-
 -- Préparation de potion de protection magique dans les bonnes tables
-INSERT INTO `craft_recette` SELECT null id, 'Préparation de potion de protection magique' nom, 0 pa, 0 mp, 'protection' type, 50 difficulte, (SELECT concat('o', `id`, '-1') FROM `objet` WHERE `nom` = 'Potion de protection magique') resultat, 99999999 royaume_alchimie, 99999999 prix;
+INSERT INTO `craft_recette` (id, nom, pa, mp, `type`, difficulte, resultat, royaume_alchimie, prix)
+			 SELECT null id, 'Préparation de potion de protection magique' nom, 0 pa, 0 mp, 'protection' type, 50 difficulte, (SELECT concat('o', `id`, '-1') FROM `objet` WHERE `nom` = 'Potion de protection magique') resultat, 99999999 royaume_alchimie, 99999999 prix;
 
 insert into `craft_recette_instrument` (`id_recette`, `type`) select `id`, 'mortier' from `craft_recette` where `nom` = 'Préparation de potion de protection magique';
 
