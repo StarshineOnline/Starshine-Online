551c771b84bf8ef7e6ccd5de021edbfef7db7513
Elettar <Elettar@starshine-online.com>
2011-06-09 19:34:41 +0000

    - Coffre III (bug #457)

---
+++ sql/update_20110609.sql
@@ -0,0 +1,14 @@
+INSERT INTO `terrain_batiment` (
+`id` ,
+`nom` ,
+`description` ,
+`type` ,
+`effet` ,
+`nb_case` ,
+`prix` ,
+`requis` ,
+`point_structure`
+)
+VALUES (
+NULL , 'Coffre III', '', 'coffre', '30', '3', '0', '3', '45000'
+);
