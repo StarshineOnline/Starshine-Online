ee9e306821b3206a79564622a50d9cf25f801826
maeryth <maeryth@starshine-online.com>
2011-06-18 14:16:02 +0000

    un achiev'

---
+++ sql/update_20110618.sql
@@ -0,0 +1,2 @@
+INSERT INTO achievement_type (id, nom, description, value, variable, secret, strong, color) VALUES 
+(NULL, 'Divided we fall', 'être mort sans appartenir à un groupe', '', '', '0', '0', NULL);
