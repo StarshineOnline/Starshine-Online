870894ebfa0c6f182a8c11f4c2cb935f05937b6e
bastien <bastien@starshine-online.com>
2011-06-23 14:59:54 +0000

    sql moisi

--- sql/update_20110618.sql
+++ sql/update_20110618.sql
@@ -1,2 +1,2 @@
-INSERT INTO achievement_type (id, nom, description, value, variable, secret, strong, color) VALUES 
+INSERT INTO achievement_type (id, nom, description, value, variable, secret, strong) VALUES 
 (NULL, 'Divided we fall', 'être mort sans appartenir à un groupe', '0', 'divided_fall', '0', '0');
