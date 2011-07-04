7f84a5f4c4317e06f2e2a534b491a6739ee0f667
bastien <bastien@starshine-online.com>
2011-07-04 18:17:25 +0000

    buff charisme, avec le sql complet

--- sql/update_20110704.sql
+++ sql/update_20110704.sql
@@ -20,6 +20,11 @@ INSERT INTO `comp_jeu` (
 `lvl_batiment`
 )
 VALUES (
-NULL , 'Charisme', 'Augmente de %effet% le nombre de buffs disponibles. Dure une semaine.', '20', '5', 'buff_charisme', 'distance', 'dexterite', '0', '100', 'arc', '2', '0', '604800', '8', '999', '0', '9'
+NULL , 'Charisme', 'Augmente de %effet% le nombre de buffs disponibles. Dure une semaine.', '20', '5', 'buff_charisme', 'distance', 'dexterite', '0', '100', 'arc', '2', '0', '604800', '8', 'classe:sniper', '0', '9'
 );
+
+-- don de la comp
+update perso set comp_jeu = concat(comp_jeu, ';94') where classe = 'Sniper';
+
+insert into `classe_comp_permet` (`id_classe`, `competence`, `type`) select c.id, s.id, 'comp_jeu' from classe c, comp_jeu s where s.requis = concat('classe:', c.nom);
 
