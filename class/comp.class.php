a877d21caa2fecc098ca54b393fe7fd1795a7d4a
Elettar <Elettar@starshine-online.com>
2011-09-25 12:42:21 +0000

    - les maitrises remarchent

--- class/comp.class.php
+++ class/comp.class.php
@@ -159,7 +159,7 @@ class maitrise_dague extends competence
 
   function debut_round(&$actif, &$passif) {
     $this->used = true;
-    $actif->set_potentiel_toucher($actif->set_potentiel_toucher *
+    $actif->set_potentiel_toucher($actif->get_potentiel_toucher() *
       (1 + ($actif->get_competence('maitrise_dague')->get_valeur() / 1000)));
   }
 
@@ -184,7 +184,7 @@ class maitrise_epee extends competence
 
   function debut_round(&$actif, &$passif) {
     $this->used = true;
-    $actif->set_potentiel_toucher($actif->set_potentiel_toucher *
+    $actif->set_potentiel_toucher($actif->get_potentiel_toucher() *
       (1 + ($actif->get_competence('maitrise_epee')->get_valeur() / 1000)));
     //$actif['maitrise_epee'] = $actif['competences']['maitrise_epee'];
   }
@@ -210,7 +210,7 @@ class maitrise_hache extends competence
 
   function debut_round(&$actif, &$passif) {
     $this->used = true;
-    $actif->set_potentiel_toucher($actif->set_potentiel_toucher *
+    $actif->set_potentiel_toucher($actif->get_potentiel_toucher() *
       (1 + ($actif->get_competence('maitrise_hache')->get_valeur() / 1000)));
     //$actif['maitrise_hache'] = $actif['competences']['maitrise_hache'];
   }
@@ -236,7 +236,7 @@ class maitrise_arc extends competence
 
   function debut_round(&$actif, &$passif) {
     $this->used = true;
-    $actif->set_potentiel_toucher($actif->set_potentiel_toucher *
+    $actif->set_potentiel_toucher($actif->get_potentiel_toucher() *
       (1 + ($actif->get_competence('maitrise_arc')->get_valeur() / 1000)));
     //$actif['maitrise_arc'] = $actif['competences']['maitrise_arc'];
   }
