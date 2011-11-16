f4dbb8f1da90337dbbfd22f2c30784a44ba50560
bastien <bastien@starshine-online.com>
2011-11-16 18:48:54 +0000

    debug

--- sql/update_20111114.sql
+++ sql/update_20111114.sql
@@ -1,16 +1,16 @@
-INSERT INTO `starshine`.`accessoire` (`id`, `nom`, `description`, `puissance`, `type`, `effet`, `prix`, `lvl_batiment`, `achetable`) 
+INSERT INTO `accessoire` (`id`, `nom`, `description`, `puissance`, `type`, `effet`, `prix`, `lvl_batiment`, `achetable`) 
 VALUES (NULL, 'Pierre de précision', '', '13', 'pierre_precision', '10', '0', '0', 'n');
 
-INSERT INTO `starshine`.`armure` (`id`, `nom`, `PP`, `PM`, `forcex`, `type`, `effet`, `prix`, `lvl_batiment`) 
+INSERT INTO `armure` (`id`, `nom`, `PP`, `PM`, `forcex`, `type`, `effet`, `prix`, `lvl_batiment`) 
 VALUES (NULL, 'Collier de domination animale', '0', '100', '0', 'cou', '20-5', '200000', '9');
 
-INSERT INTO `starshine`.`armure` (`id`, `nom`, `PP`, `PM`, `forcex`, `type`, `effet`, `prix`, `lvl_batiment`) 
+INSERT INTO `armure` (`id`, `nom`, `PP`, `PM`, `forcex`, `type`, `effet`, `prix`, `lvl_batiment`) 
 VALUES (NULL, 'Carapace de pierre incisive', '300', '100', '10', 'torse', '23-5', '100000', '9');
 
-INSERT INTO `starshine`.`armure` (`id`, `nom`, `PP`, `PM`, `forcex`, `type`, `effet`, `prix`, `lvl_batiment`) 
+INSERT INTO `armure` (`id`, `nom`, `PP`, `PM`, `forcex`, `type`, `effet`, `prix`, `lvl_batiment`) 
 VALUES (NULL, 'Cape d''invisibilité', '0', '100', '0', 'dos', '24-0', '100000', '9');
 
-INSERT INTO `starshine`.`armure` (`id`, `nom`, `PP`, `PM`, `forcex`, `type`, `effet`, `prix`, `lvl_batiment`) 
+INSERT INTO `armure` (`id`, `nom`, `PP`, `PM`, `forcex`, `type`, `effet`, `prix`, `lvl_batiment`) 
 VALUES (NULL, 'Collier de force', '200', '50', '0', '', '', '100000', '9');
 
-INSERT INTO `starshine`.`armure` (`id`, `nom`, `PP`, `PM`, `forcex`, `type`, `effet`, `prix`, `lvl_batiment`) VALUES (NULL, 'Casque de vision supérieure', '100', '0', '10', 'tete', '21-5', '100000', '9');
+INSERT INTO `armure` (`id`, `nom`, `PP`, `PM`, `forcex`, `type`, `effet`, `prix`, `lvl_batiment`) VALUES (NULL, 'Casque de vision supérieure', '100', '0', '10', 'tete', '21-5', '100000', '9');
