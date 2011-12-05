2f569e4370a6f803f2ae1ca8ff54dd0500a2a4e6
Martin Legry <martin.legry@gmail.com>
2011-12-05 21:05:37 +0100

    itemdonjon

---
+++ sql/update_20111205.sql
@@ -0,0 +1,1 @@
+INSERT INTO `starshine`.`arme` (`id`, `nom`, `type`, `degat`, `forcex`, `melee`, `distance`, `distance_tir`, `mains`, `var1`, `effet`, `prix`, `lvl_batiment`, `image`) VALUES (NULL, 'Mirroir éclatant', 'bouclier', '0', '10', '150', '0', '0', 'main_gauche', '', '25-10', '12', '1', '');
