2302f9d1a3977ee9cb16040aa8965dca5250cab9
Elettar <elettar@gmail.com>
2011-12-13 14:37:25 +0100

    +Dons paypal

--- don.php
+++ don.php
@@ -11,9 +11,17 @@ $ratio_don = floor(10 * ($actuel / $max));
 if($ratio_don > 10) $ratio_don = 10;
 if($ratio_don < 0) $ratio_don = 0;
 $barre_don = './image/barre/pa'.$ratio_don.'.png';*/
-/*Avancement pour paiement hébergement : <img src="<?php echo $barre_don; ? >" title="<?php echo $actuel.'€ / 250 €'; ? >" /><br />
+/*Avancement pour paiement hébergement : <img src="<?php echo $barre_don; ? >" title="<?php echo $actuel.'€ / 250 €'; ? >" /><br />*/
+?>
 <h3>Faire un don via paypal</h3>
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="CVXP4LZ8DWHV8">
+<input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !">
+<img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+</form>
+<?PHP
+/*<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="FR3RDRQTGWJEE">
 <input type="image" src="https://www.paypal.com/fr_FR/FR/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !">
