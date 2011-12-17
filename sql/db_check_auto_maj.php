f15fbbc4dcf249038662719a1f44906e80e6ecb1
Bastien Durel <bastien@durel.org>
2011-12-17 12:06:12 +0100

    auto maj

--- sql/db_check_auto_maj.php
+++ sql/db_check_auto_maj.php
@@ -17,7 +17,6 @@ $req = $db->query('select loaded from db_auto_maj');
 while ($row = $db->read_object($req)) {
   $loaded[] = $row->loaded;
 }
-var_dump($loaded);
 
 $dh = opendir(root.'sql');
 if (!$dh) die();
@@ -32,11 +31,37 @@ while (($file = readdir($dh)) !== false) {
   }
 }
 
+// use mysqli for multi_query
+$mysqli = new mysqli($cfg["sql"]['host'], $cfg["sql"]['user'],
+										 $cfg["sql"]['pass'], $cfg["sql"]['db']);
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+function verif_queries($sql) {
+	$sql2 = trim($sql); // trim spaces
+	$sql3 = trim($sql2, ";"); // remove trailing ;
+	return $sql3;
+}
+
 sort($files_to_load, SORT_STRING);
 foreach ($files_to_load as $file) {
-  $sql = 'source '.root.'sql/'.$file;
-  echo "Loading $file";
-  echo "$sql \n";
-  $sql = "insert into table db_auto_maj values ('$file')";
-  echo "$sql \n";
+  echo "Loading $file ";
+  $ct = file_get_contents(root.'sql/'.$file);
+  if (!$mysqli->multi_query(verif_queries($ct)))
+    die("\nmulti_query failed: $mysqli->error \n");
+
+  do {
+    /* store first result set */
+    if ($result = $mysqli->use_result()) {
+      $result->close();
+    }
+    echo '.';
+  } while ($mysqli->next_result());
+
+  echo "\n";
+  $sql = "insert into db_auto_maj values ('$file')";
+  $db->query($sql);
 }
