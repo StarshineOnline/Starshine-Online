e60b9c8b18813cce9fe51dbbd4d090b08117526a
Bastien Durel <bastien@durel.org>
2011-12-21 12:40:15 +0100

    petit bug encore

--- class/effect.class.php
+++ class/effect.class.php
@@ -866,7 +866,7 @@ class anneau_resistance extends effect
 	function calcul_degats(&$actif, &$passif, $degats) {
     if ($actif->get_race() == $this->effet) {
       $reduction = min(2, $degats);
-			$this->hit('L\' anneau de resistance de '.$passif->get_nom().
+			$this->hit('L\'anneau de resistance de '.$passif->get_nom().
                  ' reduit les degats de '.$reduction);
       $degats -= $reduction;
     }
@@ -874,10 +874,7 @@ class anneau_resistance extends effect
   }
   
   function calcul_degats_magiques(&$actif, &$passif, $degats, $type) {
-    if ($actif->get_race() == $this->effet) {
-       $degats = $this->inflige_degats(&$actif, &$passif, $degats);
-    }
-    return $degats;
+    return $this->calcul_degats(&$actif, &$passif, $degats);
   }
 }
 
