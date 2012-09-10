cda6b9c90d97f22e7a9ec75ad56454f1b87c6b7f
Elettar <elettar@gmail.com>
2012-09-10 15:19:02 +0200

    - Il ne faut pas mettre la base dans le fichier sql.

--- sql/update_20120910.sql
+++ sql/update_20120910.sql
@@ -1,1 +1,1 @@
-UPDATE `starshine`.`craft_recette` SET `royaume_alchimie` = '3500' WHERE nom = 'Parchemin de téléportation';
+UPDATE `craft_recette` SET `royaume_alchimie` = '3500' WHERE nom = 'Parchemin de téléportation';
