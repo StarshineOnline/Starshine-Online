53df1591de1dd5b04d429eb945a6d37028e6fe09
Elettar <elettar@gmail.com>
2013-03-06 14:10:52 +0100

    - On prennait encore en compte les niveau 0 pour la population dans certains cas

--- classement_royaume.php
+++ classement_royaume.php
@@ -8,7 +8,7 @@ include_once(root.'inc/fp.php');
 	</script>
 		<a href="classement.php" onclick="return envoiInfo(this.href, 'popup_content');">Classement des Personnages</a><br />
 		<a href="classement_groupe.php" onclick="return envoiInfo(this.href, 'popup_content');">Classement des Groupes</a><br />
-		<a href="javascript:adresse_royaume('victoire', '', '')">Victoires</a> | <a href="javascript:adresse_royaume('cases', '', '')">Cases</a> | <a href="javascript:adresse_royaume('level', '', '')">Niveaux</a> | <a href="javascript:adresse_royaume('survie', '', '')">Honneur</a> | <a href="javascript:adresse_groupe('frag', '', '')">PvP</a> | <a href="javascript:adresse_royaume('mort', '', '')">Suicide</a> | <a href="javascript:adresse_royaume('crime', '', '')">Crime</a><br />
+		<a href="javascript:adresse_royaume('victoire', '', '')">Victoires</a> | <a href="javascript:adresse_royaume('cases', '', '')">Cases</a> | <a href="javascript:adresse_royaume('level', '', '')">Niveaux</a> | <a href="javascript:adresse_royaume('honneur', '', '')">Honneur</a> | <a href="javascript:adresse_royaume('frag', '', '')">PvP</a> | <a href="javascript:adresse_royaume('mort', '', '')">Suicide</a> | <a href="javascript:adresse_royaume('crime', '', '')">Crime</a><br />
 	<?php
 	include_once(root.'classement_royaume_ajax.php');
 	?>
