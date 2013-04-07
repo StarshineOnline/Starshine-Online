aad2c0cca503f1893f3c2271c3951e3b7c425924
Bastien Durel <bastien@durel.org>
2013-04-07 21:58:24 +0200

    generation de la map admin

--- journalier2-map-monde.php
+++ journalier2-map-monde.php
@@ -25,78 +25,88 @@ function isAffichable($tab, $x, $y)
 	return false;
 }
 
+function do_map($file, $fog = true) {
+	global $db;
+	$im = imagecreate (CARTE_WIDTH, CARTE_HEIGHT)
+		or die ("Impossible d'initialiser la bibliothèque GD");
+	$background_color = imagecolorallocate ($im, 255, 255, 255);
 
-$im = imagecreate (CARTE_WIDTH, CARTE_HEIGHT)
-   or die ("Impossible d'initialiser la bibliothèque GD");
-$background_color = imagecolorallocate ($im, 255, 255, 255);
+	$color1 = imagecolorallocate($im, 0x66, 0xdd, 0x66);  ///< couleur de la plaine
+	$color2 = imagecolorallocate($im, 0x00, 0x99, 0x00);  ///< couleur de la forêt
+	$color3 = imagecolorallocate($im, 0xff, 0xff, 0x00);  ///< couleur du désert
+	$color4 = imagecolorallocate($im, 0xff, 0xff, 0xff);  ///< couleur de la banquise
+	$color5 = imagecolorallocate($im, 0x00, 0x00, 0xff);  ///< couleur de l'eau
+	$color6 = imagecolorallocate($im, 0xcc, 0xcc, 0xcc);  ///< couleur de la route
+	$color7 = imagecolorallocate($im, 0xaa, 0xaa, 0xaa);  ///< couleur des capitales
+	$color8 = imagecolorallocate($im, 0x5d, 0x43, 0x00);  ///< couleur de la montagne
+	$color9 = imagecolorallocate($im, 0x00, 0x00, 0x00);  ///< couleur des marais
+	$color11 = imagecolorallocate($im, 0x41, 0x35, 0x3e); ///< couleur de la terre maudite
+	$colorHidden = imagecolorallocate($im, 0xe3, 0xe3, 0xe3); ///< couleur case masquée
+	$show_info[0] = $color1;
+	$show_info[1] = $color1;
+	$show_info[22] = $color1;
+	$show_info[2] = $color2;
+	$show_info[3] = $color3;
+	$show_info[106] = $color3;
+	$show_info[4] = $color4;
+	$show_info[5] = $color5;
+	$show_info[85] = $color5;
+	$show_info[6] = $color8;
+	$show_info[23] = $color8;
+	$show_info[7] = $color9;
+	$show_info[8] = $color6;
+	$show_info[9] = $color6;
+	$show_info[10] = $color7;
+	$show_info[11] = $color11;
+	$show_info[16] = $color6; // donjon ?
+	$show_info[95] = $color6; // donjon ?
+	$col = 'info';
+	$carte = $file;
 
-$color1 = imagecolorallocate($im, 0x66, 0xdd, 0x66);  ///< couleur de la plaine
-$color2 = imagecolorallocate($im, 0x00, 0x99, 0x00);  ///< couleur de la forêt
-$color3 = imagecolorallocate($im, 0xff, 0xff, 0x00);  ///< couleur du désert
-$color4 = imagecolorallocate($im, 0xff, 0xff, 0xff);  ///< couleur de la banquise
-$color5 = imagecolorallocate($im, 0x00, 0x00, 0xff);  ///< couleur de l'eau
-$color6 = imagecolorallocate($im, 0xcc, 0xcc, 0xcc);  ///< couleur de la route
-$color7 = imagecolorallocate($im, 0xaa, 0xaa, 0xaa);  ///< couleur des capitales
-$color8 = imagecolorallocate($im, 0x5d, 0x43, 0x00);  ///< couleur de la montagne
-$color9 = imagecolorallocate($im, 0x00, 0x00, 0x00);  ///< couleur des marais
-$color11 = imagecolorallocate($im, 0x41, 0x35, 0x3e); ///< couleur de la terre maudite
-$colorHidden = imagecolorallocate($im, 0xe3, 0xe3, 0xe3); ///< couleur case masquée
-$show_info[0] = $color1;
-$show_info[1] = $color1;
-$show_info[22] = $color1;
-$show_info[2] = $color2;
-$show_info[3] = $color3;
-$show_info[106] = $color3;
-$show_info[4] = $color4;
-$show_info[5] = $color5;
-$show_info[85] = $color5;
-$show_info[6] = $color8;
-$show_info[23] = $color8;
-$show_info[7] = $color9;
-$show_info[8] = $color6;
-$show_info[9] = $color6;
-$show_info[10] = $color7;
-$show_info[11] = $color11;
-$col = 'info';
-$carte = 'image/carte.png';
-
-// Récupération des cases colonisées
-$requete = 'SELECT map.*
+	if ($fog) {
+		// Récupération des cases colonisées
+		$requete = 'SELECT map.*
 	FROM `map` map
 	WHERE map.x BETWEEN 1 AND '.MAP_WIDTH
-	.' AND map.y BETWEEN 1 AND '.MAP_HEIGHT
-	.' AND royaume > 0';
-$req = $db->query($requete);
-
-// Mémorisation des cases colonisées
-$casesColonisees = array(array());
-while($row = $db->read_array($req)){
-	$casesColonisees[$row['x']][$row['y']] = 1;
-}
-
-// Récupération de toutes les cases
-$requete = 'SELECT map.*
-	FROM `map` map
-	WHERE map.x BETWEEN 1 AND '.MAP_WIDTH
-	.' AND map.y BETWEEN 1 AND '.MAP_HEIGHT;
-$req = $db->query($requete);
-
-while($row = $db->read_array($req))
-{
-	// case affichable
-	if(isAffichable($casesColonisees, $row['x'], $row['y'])){
-		imagefilledrectangle($im, (($row['x'] - 1) * 3), (($row['y'] - 1) * 3),
-		((($row['x'] - 1) * 3) + 2),
-		((($row['y'] - 1) * 3) + 2),
-		$show_info[$row[$col]]);
-	}else{
-		imagefilledrectangle($im, (($row['x'] - 1) * 3), (($row['y'] - 1) * 3),
-		((($row['x'] - 1) * 3) + 2),
-		((($row['y'] - 1) * 3) + 2),
-		$colorHidden);
+			.' AND map.y BETWEEN 1 AND '.MAP_HEIGHT
+			.' AND royaume > 0';
+		$req = $db->query($requete);
+		
+		// Mémorisation des cases colonisées
+		$casesColonisees = array(array());
+		while($row = $db->read_array($req)){
+			$casesColonisees[$row['x']][$row['y']] = 1;
+		}
 	}
+
+	// Récupération de toutes les cases
+	$requete = 'SELECT map.*
+	FROM `map` map
+	WHERE map.x BETWEEN 1 AND '.MAP_WIDTH
+		.' AND map.y BETWEEN 1 AND '.MAP_HEIGHT;
+	$req = $db->query($requete);
+	
+	while($row = $db->read_array($req))
+	{
+		// case affichable
+		if (!$fog || isAffichable($casesColonisees, $row['x'], $row['y'])) {
+			imagefilledrectangle($im, (($row['x'] - 1) * 3), (($row['y'] - 1) * 3),
+													 ((($row['x'] - 1) * 3) + 2),
+													 ((($row['y'] - 1) * 3) + 2),
+													 $show_info[$row[$col]]);
+		}
+		else {
+			imagefilledrectangle($im, (($row['x'] - 1) * 3), (($row['y'] - 1) * 3),
+													 ((($row['x'] - 1) * 3) + 2),
+													 ((($row['y'] - 1) * 3) + 2),
+													 $colorHidden);
+		}
+	}
+	imagepng ($im, $carte);
+	imagedestroy($im);
 }
-imagepng ($im, $carte);
-imagedestroy($im);
+
+do_map('image/carte.png', true);
+do_map('admin/image/carte.png', false);
 
 ?>
