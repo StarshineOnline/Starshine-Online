ead6ce4b1b53498178dbce478f5c89414a0f541e
Bastien Durel <bastien@durel.org>
2013-05-06 18:22:51 +0200

    test

--- infoperso.php
+++ infoperso.php
@@ -127,7 +127,7 @@ if($joueur->get_groupe() != 0)
 {//-- Affichage du groupe si le joueur est groupé
 	if(!isset($groupe)) $groupe = new groupe($joueur->get_groupe());
 
-	echo "<div id='joueur_groupe'>
+	echo "<div id='joueur_groupe'><div id='joueur_groupe_container'>
 			<div id='joueur_groupe_bouton'>
 		   <div id='info_groupe' title='Voir les informations de mon groupe.' onclick=\"return envoiInfo('infogroupe.php?id=".$groupe->get_id()."', 'information');\"></div>
 		   <div id='mail_groupe' title=\"Envoyer un message à l'ensemble du groupe.\" onclick=\"return envoiInfo('envoimessage.php?id_type=g".$groupe->get_id()."', 'information');\"></div>
@@ -203,7 +203,7 @@ if($joueur->get_groupe() != 0)
 		}
 	}
 	echo " </ul>
-		  </div>";
+		  </div></div>";
 }
 else
 {
