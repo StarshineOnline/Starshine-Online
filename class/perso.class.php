67211a5766662e2ce68e0af70890c4afbf44d585
Elettar <elettar@gmail.com>
2013-06-27 15:34:11 +0200

    - correction d'un bug deans le partage des gain en PvP

--- class/perso.class.php
+++ class/perso.class.php
@@ -3237,7 +3237,7 @@ class perso extends entite
 			if($ennemi->get_groupe() > 0)
 			{
 				$groupe = new groupe($ennemi->get_groupe());
-				$groupe->get_share_xp($ennemi->get_pos(), $this->level());
+				$groupe->get_share_xp($ennemi->get_pos(), $this->get_level());
 				//Si on tape un joueur de son groupe xp = 0
 				foreach($groupe->membre_joueur as $membre_id)
 				{
