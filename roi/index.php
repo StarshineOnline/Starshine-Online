9615410b0fe08bc9b6d7d275b4887c35c6066e11
Bastien Durel <bastien@durel.org>
2013-10-07 20:31:39 +0200

    correction erreur js

--- roi/index.php
+++ roi/index.php
@@ -66,8 +66,8 @@ if(($joueur->get_race() == $royaume->get_race() && $joueur->get_grade()->get_id(
 	{ // Validation d'une bataille
 	echo "		
 	function validation_bataille()
-	{		
-		data = 'nom=' + $('#nom').val() + '&description=' + $('#description').val() + '&x=' + $('#x').val() + '&y=' + $('#y').val() + '&new2' ";
+	{
+		data = 'nom=' + $('#nom').val() + '&description=' + $('#description').val() + '&x=' + $('#x').val() + '&y=' + $('#y').val() + '&new2'; ";
 		$requete = "SELECT groupe.id as groupeid, groupe.nom as groupenom, groupe_joueur.id_joueur, perso.nom, perso.race FROM groupe LEFT JOIN groupe_joueur ON groupe.id = groupe_joueur.id_groupe LEFT JOIN perso ON groupe_joueur.id_joueur = perso.ID WHERE groupe_joueur.leader = 'y' AND perso.race = '".$joueur->get_race()."'";
 		$req = $db->query($requete);
 		while($row = $db->read_assoc($req))
@@ -82,7 +82,7 @@ if(($joueur->get_race() == $royaume->get_race() && $joueur->get_grade()->get_id(
 		}
 		 
 		echo "envoiInfo('gestion_bataille_new.php?'+data, 'message_confirm');
-	}
+  }
 	";
 	}
 	echo "	// ]]>
