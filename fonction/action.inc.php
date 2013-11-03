0a3d9876dfae7ea44323c0a326316042f1427810
Maeryth <martin.legry@gmail.com>
2013-11-03 17:46:50 +0100

    debug *temporaire* des bottes anti para

--- fonction/action.inc.php
+++ fonction/action.inc.php
@@ -123,7 +123,15 @@ function sub_script_action($joueur, $ennemi, $mode, &$attaque)
 		{
 			$joueur->etat['paralysie']['cpt'] = 1;
 		}
-		$resist_para = pow($joueur->get_pm_para(), 0.5)*pow($joueur->get_volonte(),1.85) + $joueur->etat['paralysie']['cpt']*1000;
+		$bonus_para = 1;
+		if ($res_para = $passif->get_bonus_permanents('resistance_para')) {
+
+-            print_debug("Ajuste la resistance à la paralysie de $res_para%<br/>");
+
+-            $bonus_para = 1 + $res_para / 100;
+
+-          }
+		$resist_para = $bonus_para*pow($joueur->get_pm_para(), 0.5)*pow($joueur->get_volonte(),1.85) + $joueur->etat['paralysie']['cpt']*1000;
 		$sm = ($ennemi->get_volonte() * $ennemi->get_sort_mort());
 							
 		$att = rand(0, $sm);
