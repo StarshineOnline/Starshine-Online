09efa33df9f4ce974c4b5dd4c343aad829a8945b
Elettar <elettar@gmail.com>
2014-01-09 19:59:11 +0100

    + On logue aussi la disparition de bâtiment lors des scripts journaliers

--- journalier4.php
+++ journalier4.php
@@ -104,6 +104,7 @@ while($row = $db->read_assoc($req))
 	if($row['bourg_id'] == '')
 	{
 		$mine = new construction($row['id']);
+    log_admin::log('journalier', 'Destruction de la construction '.$mine->get_nom().' de'.$royaume.', en '.$mine->get_x().'-'.$mine->get_y().' à cause de la disparition du bourg #'.$mine->get_rechargement() );
 		$mine->supprimer();
 	}
 }
