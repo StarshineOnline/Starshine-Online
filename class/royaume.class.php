6d9b5e4035bc858f6a3c7b7d98654beba9d7691d
Elettar <elettar@gmail.com>
2014-01-13 19:18:42 +0100

    Update royaume.class.php
    - Correction d'un bug faisant apparaitre des royaumes fantômes

--- class/royaume.class.php
+++ class/royaume.class.php
@@ -1320,7 +1320,7 @@ class royaume
 
 	function verif_hp()
 	{
-		if($this->capitale_hp <= 0 && $this->fin_raz_capitale < time())
+		if($this->id && $this->capitale_hp <= 0 && $this->fin_raz_capitale < time())
 		{
 			$this->set_capitale_hp(30000);
 			$this->sauver();
