fe33e7211bcb007089d30b272a4acc77711b5bff
Elettar <elettar@gmail.com>
2014-05-30 12:41:29 +0200

    * on modifie les effets et la durée d'enracinement (et on utilise le champ effet au lieu de effet2 pour les effets vu que la durée est gérée par le champ durée).

--- sql/update_20140529.sql
+++ sql/update_20140529.sql
@@ -1,4 +1,4 @@
-UPDATE `sort_jeu` SET `description` = 'Effectuer un déplacement vous coutera %effet2% PA supplémentaire(s)' WHERE `type` ='debuff_enracinement';
-Update `sort_jeu` SET `effet2` = 1 WHERE `type` ='debuff_enracinement';
-Update `sort_jeu` SET `effet` = 240 WHERE `nom` ='Enracinement 2';
-update `sort_jeu` SET `effet` = 240, `effet2` = 2 WHERE `nom` = 'Enracinement 3';
+UPDATE `sort_jeu` SET `description` = 'Effectuer un déplacement vous coutera %effet% PA supplémentaire(s)', duree = 21600 WHERE `type` ='debuff_enracinement';
+Update `sort_jeu` SET `effet` = 1 WHERE `nom` ='Enracinement';
+Update `sort_jeu` SET `effet` = 2 WHERE `nom` ='Enracinement 2';
+update `sort_jeu` SET `effet` = 3 WHERE `nom` = 'Enracinement 3';
