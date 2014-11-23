5740a5f6ffa1848f13540b3f06f65897516de1a5
elettar <elettar@gmail.com>
2014-11-23 16:39:20 +0100

    - correction d'erreurs dans le sql

--- sql/update_v09_aide.sql
+++ sql/update_v09_aide.sql
@@ -15,11 +15,11 @@ CREATE TABLE IF NOT EXISTS `aide` (
 -- Contenu de l'aide (à remplir)
 INSERT INTO `aide` (`id`, `titre`, `texte`, `position`, `index_tuto`, `total_tuto`, `precedant_tuto`, `suivant_tuto`) VALUES ('barre_menu', 'Titre à trouver', 'Aide à rédiger', 'bottom', 1, 2, '', 'depl_disque');
 INSERT INTO `aide` (`id`, `titre`, `texte`, `position`, `index_tuto`, `total_tuto`, `precedant_tuto`, `suivant_tuto`) VALUES ('depl_disque', 'Titre à trouver', 'Aide à rédiger', 'right', 2, 2, 'barre_menu', '');
-INSERT INTO `aide` (`titre`, `texte`, `position`) VALUES ('infos_vie', 'Titre à trouver', 'Aide à rédiger', 'bottom');
-INSERT INTO `aide` (`titre`, `texte`, `position`) VALUES ('infos_perso', 'Titre à trouver', 'Aide à rédiger', 'bottom');
-INSERT INTO `aide` (`titre`, `texte`, `position`) VALUES ('perso_heure', 'Titre à trouver', 'Aide à rédiger', 'bottom');
-INSERT INTO `aide` (`titre`, `texte`, `position`) VALUES ('perso_position', 'Titre à trouver', 'Aide à rédiger', 'bottom');
-INSERT INTO `aide` (`titre`, `texte`, `position`) VALUES ('perso_groupe', 'Titre à trouver', 'Aide à rédiger', 'bottom');
-INSERT INTO `aide` (`titre`, `texte`, `position`) VALUES ('menu_carte', 'Titre à trouver', 'Aide à rédiger', 'bottom');
-INSERT INTO `aide` (`titre`, `texte`, `position`) VALUES ('menu_panneaux', 'Titre à trouver', 'Aide à rédiger', 'bottom');
-INSERT INTO `aide` (`titre`, `texte`, `position`) VALUES ('carte', 'Titre à trouver', 'Aide à rédiger', 'bottom');
+INSERT INTO `aide` (`id`, `titre`, `texte`, `position`) VALUES ('infos_vie', 'Titre à trouver', 'Aide à rédiger', 'bottom');
+INSERT INTO `aide` (`id`, `titre`, `texte`, `position`) VALUES ('infos_perso', 'Titre à trouver', 'Aide à rédiger', 'bottom');
+INSERT INTO `aide` (`id`, `titre`, `texte`, `position`) VALUES ('perso_heure', 'Titre à trouver', 'Aide à rédiger', 'bottom');
+INSERT INTO `aide` (`id`, `titre`, `texte`, `position`) VALUES ('perso_position', 'Titre à trouver', 'Aide à rédiger', 'bottom');
+INSERT INTO `aide` (`id`, `titre`, `texte`, `position`) VALUES ('perso_groupe', 'Titre à trouver', 'Aide à rédiger', 'bottom');
+INSERT INTO `aide` (`id`, `titre`, `texte`, `position`) VALUES ('menu_carte', 'Titre à trouver', 'Aide à rédiger', 'bottom');
+INSERT INTO `aide` (`id`, `titre`, `texte`, `position`) VALUES ('menu_panneaux', 'Titre à trouver', 'Aide à rédiger', 'bottom');
+INSERT INTO `aide` (`id`, `titre`, `texte`, `position`) VALUES ('carte', 'Titre à trouver', 'Aide à rédiger', 'bottom');
