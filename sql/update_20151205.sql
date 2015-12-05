8b3340eb22a3d846543a822eec15a8393bb1fb77
Elettar <elettar@gamil.com>
2015-12-05 22:31:23 +0100

    - corrections diverses de la bdd

---
+++ sql/update_20151205.sql
@@ -0,0 +1,4 @@
+UPDATE journal_royaume SET action = 'pose_depot' WHERE action = 'Petit drapeau';
+UPDATE objet_royaume SET nom = 'Précision excécrable' WHERE id = 94;
+UPDATE buff_batiment_def SET nom = 'Précision excécrable' WHERE id = 73;
+UPDATE quete SET requis = 'n17' WHERE id = 87;
